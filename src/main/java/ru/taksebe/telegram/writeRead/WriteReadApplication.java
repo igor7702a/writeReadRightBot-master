47192ef184e431a6ba968f595e965e72c98e9a03
i.tarasov <gklsov@mail.ru>
2022-05-18 10:32:33 +0300

    Initial commit 18052022 Already sended

--- src/main/java/ru/taksebe/telegram/writeRead/WriteReadApplication.java
+++ src/main/java/ru/taksebe/telegram/writeRead/WriteReadApplication.java
@@ -1,12 +1,19 @@
 package ru.taksebe.telegram.writeRead;
 
+import de.dentrassi.crypto.pem.PemKeyStoreProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.Security;
 
 @SpringBootApplication
 public class WriteReadApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws KeyStoreException {
+        PemKeyStoreProvider newProvider = new PemKeyStoreProvider();
+        Security.addProvider(newProvider);
         SpringApplication.run(WriteReadApplication.class, args);
     }
 }
