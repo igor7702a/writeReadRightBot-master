168fbbc99ef02ec374a14a730a5a2033e0f7767d
i.tarasov <gklsov@mail.ru>
2022-05-27 10:30:37 +0300

    Initial commit 27052022 swagger

--- src/main/java/ru/taksebe/telegram/writeRead/telegram/WebhookController.java
+++ src/main/java/ru/taksebe/telegram/writeRead/telegram/WebhookController.java
@@ -1,5 +1,8 @@
 package ru.taksebe.telegram.writeRead.telegram;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +12,11 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
 @AllArgsConstructor
+@Tag(name="Название контроллера", description="Контроллер доступа к телеграм")
 public class WebhookController {
     private final WriteReadBot writeReadBot;
 
+    @Operation(summary = "Основной метод контроллера", description = "Входная точка для телеграм")
     @PostMapping("/")
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
         BotApiMethod<?> myResBAM = writeReadBot.onWebhookUpdateReceived(update);
